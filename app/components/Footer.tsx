6c467ea1679b07653c31b6af4029e65db4c1291c
Chimoneg27 <chimonegarvin27@gmail.com>
2025-08-06 10:48:21 +0200

    create first components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,48 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import { View, Text, StyleSheet } from "react-native";
+
+const Footer = () => {
+  return (
+    <View style={footerStyles.footer}>
+      <View>
+        <Feather name="settings" size={24} color="orange" />
+        <Text style={footerStyles.footerText}>Settings</Text>
+      </View>
+
+      <View>
+        <AntDesign name="qrcode" size={24} color="orange" />
+        <Text style={footerStyles.footerText}>QR Code</Text>
+      </View>
+
+      <View>
+        <AntDesign name="arrowright" size={24} color="orange" />
+        <Text style={footerStyles.footerText}>Send</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Footer;
+
+const footerStyles = StyleSheet.create({
+  footer: {
+    height: 60,
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    paddingHorizontal: 20,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+    padding: 6,
+  },
+  footerText: {
+    fontSize: 12,
+    marginTop: 5,
+    color: "black",
+  },
+});
---
+++ app/components/Header.tsx
@@ -0,0 +1,17 @@
+import { View, StyleSheet } from "react-native";
+
+const Header = () => {
+  return <View style={headerStyles.header}></View>;
+};
+
+const headerStyles = StyleSheet.create({
+  header: {
+    height: 50,
+    backgroundColor: "#354e90ff",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+  },
+});
+
+export default Header
