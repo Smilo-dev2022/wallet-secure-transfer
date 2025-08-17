6537a118c218f94b57a5774d39651b3ba4d051c0
Chimoneg27 <chimonegarvin27@gmail.com>
2025-08-17 23:23:38 +0200

    implement transfer money feature

--- app/styles/sendMoneyStyles.ts
+++ app/styles/sendMoneyStyles.ts
@@ -4,49 +4,65 @@ const sendMoneyStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#37539F",
-    padding: 20,
+    paddingHorizontal: 20,
+    paddingTop: 10,
+    alignItems: "center",
   },
-  backBtn: {
-    marginBottom: 10,
-  },
-  backText: {
-    color: "#fff",
-    fontSize: 16,
+  scrollContent: {
+    flexGrow: 1,
+    backgroundColor: "#37539F",
+    alignItems: "center",
+    paddingBottom: 20,
   },
   pageTitle: {
     fontSize: 28,
     fontWeight: "bold",
     color: "#fff",
-    marginBottom: 30,
+    marginBottom: 20,
   },
   inputCard: {
     backgroundColor: "#fff",
     borderRadius: 12,
     padding: 15,
     marginBottom: 20,
+    width: "100%",
+    maxWidth: 480,
   },
   label: {
     fontSize: 14,
     fontWeight: "600",
     color: "#37539F",
-    marginBottom: 5,
+    marginBottom: 8,
+  },
+  picker: {
+    height: 44,
+    width: "100%",
+    marginBottom: 12,
+    color: "#37539F",
   },
   input: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#E5E7EB",
+    paddingVertical: 12,
+    paddingHorizontal: 14,
+    borderRadius: 8,
     fontSize: 16,
     color: "#000",
+    marginBottom: 12,
   },
   sendBtn: {
     backgroundColor: "#F97316",
-    paddingVertical: 15,
+    paddingVertical: 14,
     borderRadius: 8,
     alignItems: "center",
-    marginTop: 20,
+    marginTop: 8,
   },
   sendBtnText: {
     color: "#fff",
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: "600",
   },
 });
 
-export default sendMoneyStyles
+export default sendMoneyStyles;
