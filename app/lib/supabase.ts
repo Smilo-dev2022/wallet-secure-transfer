46de1ea439ea8b25b43b151a071d499ae65c57c1
Chimoneg27 <chimonegarvin27@gmail.com>
2025-08-17 23:23:11 +0200

    add verification fucntion

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,10 +1,11 @@
-import { AppState, Platform } from 'react-native'
-import 'react-native-url-polyfill/auto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient, processLock } from '@supabase/supabase-js'
+import { AppState, Platform, Alert } from "react-native";
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient, processLock } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://ezlffftgfnhagzrnhtjn.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6bGZmZnRnZm5oYWd6cm5odGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2OTM4MjIsImV4cCI6MjA2OTI2OTgyMn0.tdFYzYcLf6pggfeglhcOG1Wf-9OPfdhevwc0Vl9C64c'
+const supabaseUrl = "https://ezlffftgfnhagzrnhtjn.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6bGZmZnRnZm5oYWd6cm5odGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2OTM4MjIsImV4cCI6MjA2OTI2OTgyMn0.tdFYzYcLf6pggfeglhcOG1Wf-9OPfdhevwc0Vl9C64c";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
@@ -14,7 +15,7 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     detectSessionInUrl: false,
     lock: processLock,
   },
-})
+});
 
 // Tells Supabase Auth to continuously refresh the session automatically
 // if the app is in the foreground. When this is added, you will continue
@@ -22,13 +23,57 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 // `SIGNED_OUT` event if the user's session is terminated. This should
 // only be registered once.
 if (Platform.OS !== "web") {
-  AppState.addEventListener('change', (state) => {
-    if (state === 'active') {
-      supabase.auth.startAutoRefresh()
+  AppState.addEventListener("change", (state) => {
+    if (state === "active") {
+      supabase.auth.startAutoRefresh();
     } else {
-      supabase.auth.stopAutoRefresh()
+      supabase.auth.stopAutoRefresh();
     }
-  })
+  });
 }
 
-export default supabase
+export async function verifyRecipient(identifier: string, identifierType: string) {
+  try {
+    const { data, error } = await supabase.functions.invoke('verify-recipient', {
+      body: {
+        identifier: identifier,
+        identifierType: identifierType
+      }
+    })
+
+    if (error) throw error
+    return data
+    // Contains {exists: boolean, userId?: string, fullName?: string}
+  } catch (error: any) {
+    Alert.alert('Verification error:', error)
+    throw error
+  }
+}
+
+export async function transferMoney(id: string, emailOrPhone: string, recipientIdentifierType:string, amount: number, desc?: string) {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    console.error("User not authenticated");
+    return;
+  }
+  const { data, error } = await supabase.functions.invoke("transfer-money", {
+    body: {
+      senderId: id,
+      recipientIdentifier: emailOrPhone,
+      recipientIdentifierType: recipientIdentifierType, // or phone
+      amount: amount,
+      description: desc
+    },
+  });
+
+  if (error) {
+    Alert.alert('Error', error.message)
+  } else {
+    Alert.alert("Transfer sucessful:", data)
+  }
+}
+
+export default supabase;
